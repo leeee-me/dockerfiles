4d70fd84376519e2432eecc5a3cd988d6f91e7b2
leeee-me <41778946+leeee-me@users.noreply.github.com>
2026-03-06 00:52:26 +0800

    add quiet mode for grc-sstakw

--- grc-tools/grc-sstakw.sh
+++ grc-tools/grc-sstakw.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
 
-HOME=/home/me
+HOME=/home/leeee
 
 STAKETIME=$1
 [ -z "$1" ] && STAKETIME=86400
 
 /bin/ps -axfww | /bin/grep -v grep | /bin/grep -q gridcoinresearch; [ ${PIPESTATUS[2]} -ne 0 ] && echo "GridcoinResearchD is gone" && exit 1
 
-echo ".. lock wallet"
+[ ! -z "$2" ] && echo ".. lock wallet"
 /usr/bin/gridcoinresearchd walletlock || exit 1
-echo "... unlock wallet for staking by $STAKETIME secs"
+[ ! -z "$2" ] && echo "... unlock wallet for staking by $STAKETIME secs"
 /usr/bin/gridcoinresearchd walletpassphrase `cat $HOME/.passme.gpg.asc | gpg -d 2> /dev/null` $STAKETIME true
 
-echo -n ".... sleep 15s to update" 1>&2 && for i in `seq 15`; do echo -n "." 1>&2; sleep 1s; done && echo 1>&2
-echo
+[ ! -z "$2" ] && echo -n ".... sleep 15s to update" 1>&2
+[ ! -z "$2" ] && for i in `seq 15`; do echo -n "." 1>&2; sleep 1s; done && echo 1>&2
+[ ! -z "$2" ] && echo
 
-echo "=== beacon"
-/usr/bin/gridcoinresearchd beaconstatus | /usr/bin/jq -r '.active[] | keys_unsorted[] as $k | "\($k), \(.[$k])"' | grep -e ^cpid -e ^active -e ^address -e ^is_mine
+[ ! -z "$2" ] && echo "=== beacon"
+[ ! -z "$2" ] && /usr/bin/gridcoinresearchd beaconstatus | /usr/bin/jq -r '.active[] | keys_unsorted[] as $k | "\($k), \(.[$k])"' | grep -e ^cpid -e ^active -e ^address -e ^is_mine
 
-echo "=== mining"
-/usr/bin/gridcoinresearchd getmininginfo | /usr/bin/jq -r 'keys_unsorted[] as $k | "\($k), \(.[$k])"' | grep -e ^staking -e ^mining-error 
+[ ! -z "$2" ] && echo "=== mining"
+[ ! -z "$2" ] && /usr/bin/gridcoinresearchd getmininginfo | /usr/bin/jq -r 'keys_unsorted[] as $k | "\($k), \(.[$k])"' | grep -e ^staking -e ^mining-error  -e ^BoincRewardPending -e ^researcher_status
 
-echo "=== info"
-/usr/bin/gridcoinresearchd getinfo | /usr/bin/jq -r 'keys_unsorted[] as $k | "\($k), \(.[$k])"' | grep -e '^connections' -e '^version' -e '^walletversion' -e '^balance'
+[ ! -z "$2" ] && echo "=== info"
+[ ! -z "$2" ] && /usr/bin/gridcoinresearchd getinfo | /usr/bin/jq -r 'keys_unsorted[] as $k | "\($k), \(.[$k])"' | grep -e '^connections' -e '^version' -e '^walletversion' -e '^balance'
 
-echo "=== wallet"
-/usr/bin/gridcoinresearchd getwalletinfo | /usr/bin/jq -r 'keys_unsorted[] as $k | "\($k), \(.[$k])"' | grep -e '^staking'
+[ ! -z "$2" ] && echo "=== wallet"
+[ ! -z "$2" ] && /usr/bin/gridcoinresearchd getwalletinfo | /usr/bin/jq -r 'keys_unsorted[] as $k | "\($k), \(.[$k])"' | grep -e '^staking'
 
 
-echo
-echo -n ">> Wallet unlocked until: "
-date --date=@`/usr/bin/gridcoinresearchd getinfo | /usr/bin/jq -r '.unlocked_until'`
-echo -n ">> Now time is:           "
-date --date=now
+[ ! -z "$2" ] && echo
+[ ! -z "$2" ] && echo -n ">> Wallet unlocked until: " && date --date=@`/usr/bin/gridcoinresearchd getinfo | /usr/bin/jq -r '.unlocked_until'`
+[ ! -z "$2" ] && echo -n ">> Now time is:           " && date --date=now
